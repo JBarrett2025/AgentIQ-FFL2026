293920a91ad9532b125a5ec1f08df7a38136f64c
Joe Barrett <jbbaesis@gmail.com>
2026-06-15 11:21:24 -0400

    chore: specify Node runtime and use firebase-functions/v1 compat

--- functions/index.ts
+++ functions/index.ts
@@ -1,4 +1,4 @@
-const functions = require('firebase-functions');
+const functions = require('firebase-functions/v1');
 const admin = require('firebase-admin');
 const { v2 } = require('@google-cloud/translate');
 const { GoogleGenAI, Type } = require('@google/genai');
